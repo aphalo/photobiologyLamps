169121447ea6f9a95f5c455e75b59c94676fe593
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-05-09 20:28:03 +0300

    Now calc_lamp_output() calls calc_source_output() in package photobiology.

--- R/calc.lamp.output.r
+++ R/calc.lamp.output.r
@@ -3,6 +3,8 @@
 #' @description
 #' Calculate interpolated values by interpolation from user-supplied spectral emission
 #' data or by name for data included in the package, scaling the values.
+#' The use of this function is DEPRECATED as it will soon be removes. Please use
+#' calc_source_output() instead.
 #'
 #' @usage calc_lamp_output(w.length.out, lamp.name=NULL, 
 #'                                w.length.in=NULL, s.irrad.in=NULL, 
@@ -14,13 +16,11 @@
 #' @param w.length.in numeric vector of wavelengths (nm) for input
 #' @param s.irrad.in numeric vector of spectral transmittance value (fractions or percent)
 #' @param unit.in a character string "energy" or "photon"
-#' @param unit.out (DEPRECATED) a character string "energy" or "photon", if NULL (the default) unit.out is set to unit.in (which depends on lamp data, if selected with lamp.name)
+#' @param unit.out a character string "energy" or "photon", if NULL (the default) unit.out is set to unit.in (which depends on lamp data, if selected with lamp.name)
 #' @param scaled NULL, "peak", "area"; div ignored if !is.null(scaled) 
 #' @param fill if NA, no extrapolation is done, and NA is returned for wavelengths outside the range of the input. If NULL then the tails are deleted. If 0 then the tails are set to zero.
 #'  
-#' @return a dataframe with four numeric vectors with wavelength values (w.length), scaled and interpolated spectral energy irradiance (s.e.irrad), 
-#'    scaled and interpolated spectral photon irradiance values (s.q.irrad), and for backwards compatibility a vector which is equal to either s.e.irrad or s.q.irrad
-#'    depending on the argument passed to unit.out (s.irrad)
+#' @return a dataframe with four numeric vectors with wavelength values (w.length), scaled and interpolated spectral energy or photon irradiance (s.irrad) depending on the argument passed to unit.out (s.irrad)
 #' @keywords manip misc
 #' @export
 #' @importFrom photobiology check_spectrum trim_tails integrate_irradiance as_quantum_mol as_energy
@@ -40,79 +40,22 @@ calc_lamp_output <- function(w.length.out,
                                     lamp.name=NULL, 
                                     w.length.in=NULL, s.irrad.in=NULL, 
                                     unit.in="energy", unit.out=NULL,
-                                    scaled=NULL, fill=NA) {  
-  # we first check the different possible inputs and convert to
-  # two vectors w.length.in and s.irrad.in
+                                    scaled=NULL, fill=NA) { 
+  warning("The use of 'calc_lamp_output()' is deprecated, please use 'calc_source_output()' instead.")
+  # we just call calc_source_output()
+  out.data <- calc_source_output(w.length.out=w.length.out,
+                                 source.name=lamp.name, 
+                                 w.length.in=w.length.in, s.irrad.in=s.irrad.in, 
+                                 unit.in=unit.in,
+                                 scaled=scaled, fill=fill)
   
-  if (is.null(w.length.in) | is.null(s.irrad.in)) {
-    if (is.null(lamp.name)) {
-      return(NA) 
-    } else {
-      lamp.object.name <- paste(lamp.name, "data", sep=".")
-      if (!exists(lamp.object.name)) {
-        warning("No data for lamp with name: ", lamp.object.name)
-        return(NA)
-      }
-      lamp.object <- get(lamp.object.name)
-      w.length.in <- lamp.object$w.length
-      if (with(lamp.object, exists("s.e.irrad"))) {
-        unit.in <- "energy"
-        s.irrad.in <- lamp.object$s.e.irrad
-      } else if (with(lamp.object, exists("s.q.irrad"))) {
-        unit.in <- "photon"
-        s.irrad.in <- lamp.object$s.q.irrad
-      } else {
-        return(NA)
-      }
-    }
-  } else if (!check_spectrum(w.length.in, s.irrad.in)) {
-      return(NA)
-  }
-
-  if (is.null(unit.out)) unit.out <- unit.in
-  
-  # we interpolate using a spline or linear interpolation
-
-  if (length(w.length.out) < 25) {
-    # cubic spline
-    out.fill.selector <- w.length.out < w.length.in[1] | w.length.out > w.length.in[length(w.length.in)]
-    s.irrad.out[out.fill.selector] <- fill
-    s.irrad.out[!out.fill.selector] <- spline(w.length.in, s.irrad.in, xout=w.length.out[!out.fill.selector])$y
-  } else {
-    # linear interpolation
-    s.irrad.out <- approx(x = w.length.in, y = s.irrad.in, xout = w.length.out, ties = "ordered", yleft = fill, yright = fill)$y
-  }
-
-  # do scaling
-  
-  if (!is.null(scaled)) {
-    if (scaled=="peak") {
-      div <- max(s.irrad.out, na.rm=TRUE)
-    } else if (scaled=="area") {
-      s.irrad.na.sub <- s.irrad.out
-      s.irrad.na.sub[is.na(s.irrad.na.sub)] <- 0.0
-      div <- integrate_irradiance(w.length.out, s.irrad.na.sub)
-    } else {
-      warning("Ignoring unsupported scaled argument: ", scaled)
-      div <- 1.0
-    }
-    s.irrad.out <- s.irrad.out / div
-  }
-  
-  # we check unit.in and unit.out and convert the output spectrum accordingly
-
-  if (unit.in == "energy") {
-    out.data <- data.frame(w.length = w.length.out, s.e.irrad = s.irrad.out, s.q.irrad = as_quantum_mol(w.length.out, s.irrad.out))
-  } else if (unit.in == "photon") {
-    out.data <- data.frame(w.length = w.length.out, s.e.irrad = as_energy(w.length.out, s.irrad.out), s.q.irrad = s.irrad.out)
-  } else {
-    warning("Bad argument for unit.in: ", unit.in)
-    return(NA)
-  }
   if (unit.out == "energy") {
     out.data$s.irrad <- out.data$s.e.irrad
   } else if (unit.out == "photon") {
     out.data$s.irrad <- out.data$s.e.irrad
   }
+  out.data$s.e.irrad <- NULL
+  out.data$s.q.irrad <- NULL
+
   return(out.data)
 }
--- R/photobiologyLamps-package.r
+++ R/photobiologyLamps-package.r
@@ -11,8 +11,8 @@
 #' \tabular{ll}{
 #' Package: \tab photobiologyLamps\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.1.8\cr
-#' Date: \tab 2014-05-07\cr
+#' Version: \tab 0.1.9\cr
+#' Date: \tab 2014-05-09\cr
 #' License: \tab GPL (>= 3)\cr
 #' URL: \tab \url{http://uv4growth.dyndns.org},\cr \tab \url{http://openinstruments.dyndns.org},\cr
 #' \tab \url{https://bitbucket.org/aphalo/photobiology}\cr
