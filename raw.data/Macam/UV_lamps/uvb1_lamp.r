204b09dadef9d08b47b787c5ee2752c681bde156
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-11-13 19:42:32 +0200

    Edited the vignette to use plot.sources.pct and  rebuilt one object that I missed before.

--- raw.data/Macam/UV_lamps/uvb1_lamp.r
+++ raw.data/Macam/UV_lamps/uvb1_lamp.r
@@ -3,7 +3,7 @@ oldwd <- setwd("./raw.data/Macam/UV_lamps")
 
 qpanel.uvb313.data <- read.csv(file="Q-Panel_UVB-313.csv", skip=2, header=FALSE, col.names=c("w.length", "s.e.irrad"), nrows=561)
 qpanel.uvb313.spct <- e2q(setSourceSpct(qpanel.uvb313.data))
-save(qpanel.uvb313.spct, file="./data/qpanel.uvb313.spct.rda")
+save(qpanel.uvb313.spct, file="../../../data/qpanel.uvb313.spct.rda")
 
 qpanel.uva340.data <- read.csv(file="Q-Panel_UVA-340.csv", skip=2, header=FALSE, col.names=c("w.length", "s.e.irrad"), nrows=561)
 qpanel.uva340.spct <- e2q(setSourceSpct(qpanel.uva340.data))
