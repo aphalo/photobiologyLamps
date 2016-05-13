dbbe7fae17381797e781210e09ca6f1e95f3dcf7
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2016-05-13 22:21:21 +0300

    Rebuild data objects and update versions of dependencies.

--- data-raw/LI1800/build.all.licor.data.r
+++ data-raw/LI1800/build.all.licor.data.r
@@ -1,13 +1,15 @@
 library(photobiologyInOut)
+ ### NEEDS REWRITING!
+ ### 
+# oldwd <- setwd("data-raw/LI1800")
+# df.names <- read_licor_prn("./", "../../data/")
+# all.names <- NULL
+# for (str in df.names){
+#   all.names <- paste(all.names, str)
+# }
+# all.names <- paste("#\'", all.names, "\nNULL")
+# message(all.names)
+# system("cp licor.lamps.data.template.r licor.lamps.data.r")
+# cat(all.names, file="licor.lamps.data.r", append=TRUE)
+# setwd(oldwd)
 
-oldwd <- setwd("data-raw/LI1800")
-df.names <- read_licor_files("./", "../../data/", high.limit = 900, unit.out = "energy", date = NA)
-all.names <- NULL
-for (str in df.names){
-  all.names <- paste(all.names, str)
-}
-all.names <- paste("#\'", all.names, "\nNULL")
-message(all.names)
-system("cp licor.lamps.data.template.r licor.lamps.data.r")
-cat(all.names, file="licor.lamps.data.r", append=TRUE)
-setwd(oldwd)
