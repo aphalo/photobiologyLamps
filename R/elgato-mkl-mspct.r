7fa1a41d2035fc4e8eed221a3d90d3aa94a383bf
Pedro Aphalo <pedro.aphalo@helsinki.fi>
2024-11-11 02:27:07 +0200

    Add elgato_mkl_dim.mspct

--- R/elgato-mkl-mspct.r
+++ R/elgato-mkl-mspct.r
@@ -1,19 +1,27 @@
-#' @title Spectra for an Mini Key Light LED video lamp
+#' @title Spectra for an Elgato Key Light Mini LED video lamp
 #'
-#' @description A collection of lamp emission spectra for an Elgato Mini Key 
-#'   Light white bi-colour LED lamp for video and photography.
+#' @description A collection of lamp emission spectra for an Elgato Key 
+#'   Light Mini bi-colour LED lamp for video and photography.
 #'
-#' @details \code{elgato_mkl_cct.mspct} contains a collection of
+#' @details \code{elgato_klm_cct.mspct} contains a collection of
 #'   \code{"source_spct"} objects with spectral emission data for different
-#'   dimming and colour temperature settings. The lamp was maintained at the
-#'   same distance of 170 mm from the input optics of the spectrometer. Thus, the
-#'   different spectra are comparable and provided expressed in calibrated
-#'   spectral energy irradiance units.
+#'   colour temperature (CCT) settings at full power. The lamp was maintained at
+#'   the same distance of 170 mm from the input optics of the spectrometer. 
+#'   Thus, the different spectra are comparable and provided expressed in
+#'   calibrated spectral energy irradiance units.
+#'   
+#'   \code{elgato_klm_dim.mspct} contains a collection of
+#'   \code{"source_spct"} objects with spectral emission data for different
+#'   dimming settings at a CCT setting of 4000 K. The lamp was maintained at
+#'   the same distance of 165 mm from the input optics of the spectrometer. 
+#'   Thus, the different spectra are comparable and provided expressed in
+#'   calibrated spectral energy irradiance units.
 #'   
 #'   This light source uses LEDs on the edges of a diffusion panel giving very
-#'   evenly distributed diffuse light. It is based Osram warm white light and 
+#'   evenly distributed diffuse light. It is based on Osram warm white light and 
 #'   cool white LEDs. A built-in Lithium battery powers it. The mix of warm and
-#'   cool light can be adjusted as well as the dimming.
+#'   cool light can be adjusted as well as the dimming. Remote control through
+#'   Wifi is possible.
 #'
 #'   The variables in each member spectrum are as follows: \itemize{ \item
 #'   w.length (nm) \item s.e.irrad (\eqn{\mathrm{W\,m^{-2}\,nm^{-1}}}) }
@@ -34,12 +42,20 @@
 #' Aphalo, Pedro J. (2022) Small fill/video LED lights revisited: A new 
 #'   comparison. \url{https://www.photo-spectrum.info/pages/illumination/led-fill-lights-2.html}.
 #' 
-#' @format \code{elgato_mkl_cct.mspct} is a \code{"source_mspct"} object containing 
+#' @format \code{elgato_klm_cct.mspct} is a \code{"source_mspct"} object containing 
 #'   a collection of 6 \code{"source_spct"} objects as members. Members are
 #'   named.
 #' 
 #' @examples
 #' 
-#' summary(elgato_mkl_cct.mspct)
+#' summary(elgato_klm_cct.mspct)
+#' summary(elgato_klm_dim.mspct)
 #' 
-"elgato_mkl_cct.mspct"
+"elgato_klm_cct.mspct"
+
+#' @rdname elgato_klm_cct.mspct
+#' 
+#' @docType data
+#' @keywords datasets
+#' 
+"elgato_klm_dim.mspct"
--- data-raw/Maya-video-LED/make-elgato-mkl-dimming-mspct.R
+++ data-raw/Maya-video-LED/make-elgato-mkl-dimming-mspct.R
@@ -5,8 +5,8 @@ library(lubridate)
 # clear workspace
 rm(list = ls(pattern = "*"))
 
-file <- list.files(path = "data-raw/Maya-video-LED/LEDs-2022-09-30",
-                   pattern = "^collection.*\\.170mm\\.[Rr]da$",
+file <- list.files(path = "data-raw/Maya-video-LED/LEDs-2022-09-10",
+                   pattern = "^collection\\.Elgato.*\\.[Rr]da$",
                    full.names = TRUE)
 
 load(file)
@@ -15,25 +15,25 @@ rm(list = ls(pattern = "*raw.lst"))
 
 spectrum<- ls(pattern = "\\.mspct")[1]
 
-new.names <- "elgato_klm_cct.mspct"
+new.names <- "elgato_klm_dim.mspct"
 lamp.info <- "Elgato Key Light Mini"
 
 lamp.type <- "LED white bi-colour video"
 
-how.measured <- "Array spectrometer, Ocean Optics Maya 2000 Pro; Bentham cosine diffuser D7H; distance 170 mm."
+how.measured <- "Array spectrometer, Ocean Optics Maya 2000 Pro; Bentham cosine diffuser D7H; distance approx. 165 mm."
 
 temp.mspct <- get(spectrum)
-summary(temp.mspct)
+temp.mspct <- temp.mspct[-length(temp.mspct)] # remove duplicate
+summary(temp.mspct, which.metadata = "what.measured")
+
+temp.mspct <- temp.mspct[grepl("4000K", names(temp.mspct))]
 
 names(temp.mspct)
 
-names(temp.mspct) <- make.names(names(temp.mspct), unique = TRUE)
-autoplot(temp.mspct[c(11, 12)])
-temp.mspct <- temp.mspct[-12] # duplicate
-
-temp.mspct <- temp.mspct[grepl("max", names(temp.mspct))]
-
-names(temp.mspct)
+# names(temp.mspct) <- make.names(names(temp.mspct), unique = TRUE)
+# autoplot(temp.mspct[c(11, 12)])
+# temp.mspct <- temp.mspct[-12] # duplicate
+# names(temp.mspct)
 
 summary(temp.mspct, which.metadata = "what.measured")
 
@@ -42,16 +42,15 @@ member.names <- character()
 
 for (i in seq_along(temp.mspct)) {
   temp.spct <- temp.mspct[[i]]
-  what.measured <- gsub("elgato\\.|\\.spct|K", "", names(temp.mspct)[i])
-  what.measured <- gsub("(max|half)\\.([0-9]{4})", "at \\2K dimmed to \\1", what.measured)
-  what.measured <- gsub("max", "100pc", what.measured)
-  what.measured <- gsub("half", "50pc", what.measured)
+  what.measured <- gsub("elgato\\.|\\.spct", "", names(temp.mspct)[i])
+  what.measured <- gsub("K\\.", " dimmed to ", what.measured)
+  what.measured <- gsub("4000", "at 4000K", what.measured)
   member.names <- gsub(" ", ".", c(member.names, what.measured))
   what.measured <- paste(lamp.type, "lamp:", lamp.info, what.measured)
   # temp.spct <- normalize(temp.spct)
   temp.spct <- smooth_spct(temp.spct, method = "supsmu")
   temp.spct <- thin_wl(temp.spct, max.slope.delta = 0.0005)
-  temp.spct <- trim_wl(temp.spct, c(375, NA), fill = 0)
+  temp.spct <- trim_wl(temp.spct, c(400, NA), fill = 0)
   setHowMeasured(temp.spct, how.measured)
   setWhatMeasured(temp.spct, what.measured)
   comment(temp.spct) <- NULL
@@ -63,12 +62,12 @@ for (i in seq_along(temp.mspct)) {
 }
 assign(new.names, temp.mspct)
 
-names(elgato_klm_cct.mspct) <- member.names
-summary(elgato_klm_cct.mspct)
-what_measured(elgato_klm_cct.mspct)
+names(elgato_klm_dim.mspct) <- member.names
+summary(elgato_klm_dim.mspct)
+what_measured(elgato_klm_dim.mspct)
 
-print(autoplot(elgato_klm_cct.mspct))
-q_irrad(elgato_klm_cct.mspct[["at.4000K.dimmed.to.100pc"]], scale.factor = 1e6)
+print(autoplot(elgato_klm_dim.mspct))
+q_irrad(elgato_klm_dim.mspct[["at.4000K.dimmed.to.100pc"]], scale.factor = 1e6)
 
-save(elgato_klm_cct.mspct, file = "data/elgato-klm-cct-mspct.rda")
+save(elgato_klm_dim.mspct, file = "data/elgato-klm-dim-mspct.rda")
 
